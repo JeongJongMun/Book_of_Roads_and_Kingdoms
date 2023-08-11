f94ae30572e27325ddb330aa76ba7dc5a03d318c
ramuram3 <chuntaps_@naver.com>
2023-08-11 19:50:07 +0900

    0811 1948 HR
    1. 플레이어 이동

---
+++ Assets/Scenes/0Scripts/Player.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float hitTime;
+    public bool isHit;
+
+
+    Rigidbody2D rigid;
+    public Animator animator;
+    SpriteRenderer spriter;
+    public RuntimeAnimatorController[] animatorControllers;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        Move();
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+
+
+
+
+
+    void Move()
+    {
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float yAxis = Input.GetAxisRaw("Vertical");
+        Debug.Log(xAxis);
+    
+        Vector2 moveVec = new Vector2(xAxis,yAxis).normalized * moveSpeed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + moveVec);
+        if (moveVec.x != 0)
+        {
+            spriter.flipX = moveVec.x > 0 ? false : true;
+        }
+    }
+
+
+
+
+
+
+}
